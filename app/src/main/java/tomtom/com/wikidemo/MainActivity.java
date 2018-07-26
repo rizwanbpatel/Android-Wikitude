95f790d7aa9f072ae572438335d8b8e5bcb9b813
gulve <abhijeet.gulve@tomtom.com>
2018-07-26 12:05:10 +0530

    remove hardcoded latlong

--- app/src/main/java/tomtom/com/wikidemo/MainActivity.java
+++ app/src/main/java/tomtom/com/wikidemo/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         this.architectView = (ArchitectView) this.findViewById(R.id.architectView);
         final ArchitectStartupConfiguration config = new ArchitectStartupConfiguration();
         config.setLicenseKey("4bHXRiTfwAXTCeH60c0HKLoi3twDKxItUd56r9isF7hZ04fIqsRxuBMAAondByGMjFAJzJdPaR3/3Ctm8VAP9QMbX/PAnMlLLzynGK9jrbbo19v4oi7t1EoEnbqcl+n24qewNvyanZuuch5wf4889PFmAJTCjVsR5eKmysUxMGNTYWx0ZWRfX8eaqYtW7fSolueGvBUhzkcavmdjGjPO7V+YLGUNbNFK8kTZhjvLWn+nmMl5KZPNvsMkckPzpLaoRiys3Jn3v0KwaM4zsqyGxOMV7zuaETbfoC7iNZ7N4SsSIZHfk+DChZY/9KOcODp76Wu9xA5g6a2EUNfy3aXb+X+VbxSJXUn7vuky5ddnl9z7igKzjT7zGCbkCGd0iOTAZEdkAf2iyz7/oV5zBaTVi1L9twslvFCDGf1OGUKPhh363tnH/ubkaTr4SuV0Vj6EtYhUuU0vJleDLpIo8iJHwo9JeHoyAKvhCT7AlKXsS64U1dKhfpWyXQI7SmnF211OzU5xHd82E967SJzD9UpHg2IEou3V1FYDAye6cCYW6alBSOljIE9MQsIsDPC/VwXjqdz9E7d10YSI/hDfPI419KcDFqRQbn6N1m9oQid3ptaoDhjPMnATRt79OFCDaNxAk9Nv7JaZNM3l5uXgCgf5FWM2wnpzOn6DyZGgqaBvEZzZ8AhAiyRrpXupcEPmU+s4QJUqwbRPHLqa+o6EM1nhRQ==");
-        
+
         // Acquire a reference to the system Location Manager
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -42,10 +42,10 @@ public class MainActivity extends AppCompatActivity {
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
-                if (location!=null && MainActivity.this.architectView != null ) {
+                if (location != null && MainActivity.this.architectView != null) {
                     // check if location has altitude at certain accuracy level & call right architect method (the one with altitude information)
-                    if ( location.hasAltitude() && location.hasAccuracy() && location.getAccuracy()<7) {
-                        MainActivity.this.architectView.setLocation( location.getLatitude(), location.getLongitude(), location.getAltitude(), location.getAccuracy() );
+                    if (location.hasAltitude() && location.hasAccuracy()) {
+                        MainActivity.this.architectView.setLocation(location.getLatitude(), location.getLongitude(), location.getAltitude(), location.getAccuracy());
                     } else {
                         MainActivity.this.architectView.setLocation( location.getLatitude(), location.getLongitude(), location.hasAccuracy() ? location.getAccuracy() : 1000 );
                     }
@@ -78,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
         this.architectView.onPostCreate();
         try {
             this.architectView.load("index.html");
-            architectView.setLocation(18.5559569,73.8918798,5);
         } catch (IOException e) {
             e.printStackTrace();
         }

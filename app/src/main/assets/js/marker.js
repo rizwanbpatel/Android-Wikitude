c35d341dd72798e747502c3048fdb1989a8b6aa3
Vyankat Sattarwar <vyankat.sattarwar@tomtom.com>
2018-08-15 14:13:32 +0530

    Added raddar and range functionality

--- app/src/main/assets/js/marker.js
+++ app/src/main/assets/js/marker.js
@@ -1,15 +1,15 @@
 var kMarker_AnimationDuration_ChangeDrawable = 500;
 var kMarker_AnimationDuration_Resize = 1000;
 
-//#23B9FF
+
 function Marker(poiData) {
 
     this.poiData = poiData;
     this.isSelected = false;
 
     /*
-     With AR.PropertyAnimations you are able to animate almost any property of ARchitect objects. This sample will animate the opacity of both background drawables so that one will fade out while the other one fades in. The scaling is animated too. The marker size changes over time so the labels need to be animated too in order to keep them relative to the background drawable. AR.AnimationGroups are used to synchronize all animations in parallel or sequentially.
-     */
+        With AR.PropertyAnimations you are able to animate almost any property of ARchitect objects. This sample will animate the opacity of both background drawables so that one will fade out while the other one fades in. The scaling is animated too. The marker size changes over time so the labels need to be animated too in order to keep them relative to the background drawable. AR.AnimationGroups are used to synchronize all animations in parallel or sequentially.
+    */
 
     this.animationGroup_idle = null;
     this.animationGroup_selected = null;
@@ -18,132 +18,161 @@ function Marker(poiData) {
     // create the AR.GeoLocation from the poi data
     var markerLocation = new AR.GeoLocation(poiData.latitude, poiData.longitude, poiData.altitude);
 
-    // create an AR.ImageDrawable for the marker in idle state
-    this.markerDrawable_idle = new AR.ImageDrawable(World.markerDrawable_idle, 5, {
-        zOrder: 0,
-        opacity: 0.25,
-        /*
-         To react on user interaction, an onClick property can be set for each AR.Drawable. The property is a function which will be called each time the user taps on the drawable. The function called on each tap is returned from the following helper function defined in marker.js. The function returns a function which checks the selected state with the help of the variable isSelected and executes the appropriate function. The clicked marker is passed as an argument.
-         */
-        onClick: Marker.prototype.getOnClickTrigger(this)
-    });
+   // create an AR.ImageDrawable for the marker in idle state
+       this.markerDrawable_idle = new AR.ImageDrawable(World.markerDrawable_idle, 5, {
+           zOrder: 0,
+           opacity: 0.25,
+           /*
+            To react on user interaction, an onClick property can be set for each AR.Drawable. The property is a function which will be called each time the user taps on the drawable. The function called on each tap is returned from the following helper function defined in marker.js. The function returns a function which checks the selected state with the help of the variable isSelected and executes the appropriate function. The clicked marker is passed as an argument.
+            */
+           onClick: Marker.prototype.getOnClickTrigger(this)
+       });
 
     // create an AR.ImageDrawable for the marker in selected state
-    this.markerDrawable_selected = new AR.ImageDrawable(World.markerDrawable_selected, 5, {
-        zOrder: 0,
-        opacity: 1.0,
-        onClick: null
-    });
-    this.imageLable = null;
-    if (poiData.category.toUpperCase().includes("RESTAURANT")) {
-        this.imageLable = World.hotel;
-    } else if (poiData.category.toUpperCase().includes("NIGHTLIFE")) {
-        this.imageLable = World.cafe;
-    }
-    else if (poiData.category.toUpperCase().includes("CAFE_PUB")) {
-        this.imageLable = World.cafe;
-    } else if (poiData.description.toUpperCase().includes("PHARMACY")) {
-        this.imageLable = World.doctor;
-    }
-    else if (poiData.category.toUpperCase().includes("DOCTOR")) {
-        this.imageLable = World.doctor;
-    } else if (poiData.category.toUpperCase().includes("PLACE_OF_WORSHIP")) {
-        this.imageLable = World.temple;
-    } else if (poiData.category.toUpperCase().includes("RESIDENTIAL_ACCOMMODATION")) {
-        this.imageLable = World.residential;
-    } else if (poiData.category.toUpperCase().includes("GOVERNMENT_OFFICE")) {
-        this.imageLable = World.gov;
-    }
-    else if (poiData.category.toUpperCase().includes("BANK")) {
-        this.imageLable = World.bank;
-    } else if (poiData.category.toUpperCase().includes("SHOP")) {
+        this.markerDrawable_selected = new AR.ImageDrawable(World.markerDrawable_selected, 5, {
+            zOrder: 0,
+            opacity: 1.0,
+            onClick: null
+        });
         this.imageLable = null;
-    } else if (poiData.category.toUpperCase().includes("CASH_DISPENSER")) {
-        this.imageLable = World.bank;
-    }
-    else if (poiData.category.toUpperCase().includes("SCHOOL")) {
-        this.imageLable = World.school;
-    }
-    else if (poiData.category.toUpperCase().includes("POST_OFFICE")) {
-        this.imageLable = World.post_office;
-    }else if (poiData.category.toUpperCase().includes("HOTEL_MOTEL")) {
-        this.imageLable = World.hotel;
-    }else if (poiData.category.toUpperCase().includes("AIRPORT")) {
-        this.imageLable = World.airport;
-    }else {
-        this.imageLable = World.default;
-    }
+        if (poiData.category.toUpperCase().includes("RESTAURANT")) {
+            this.imageLable = World.hotel;
+        } else if (poiData.category.toUpperCase().includes("NIGHTLIFE")) {
+            this.imageLable = World.cafe;
+        }
+        else if (poiData.category.toUpperCase().includes("CAFE_PUB")) {
+            this.imageLable = World.cafe;
+        } else if (poiData.description.toUpperCase().includes("PHARMACY")) {
+            this.imageLable = World.doctor;
+        }
+        else if (poiData.category.toUpperCase().includes("DOCTOR")) {
+            this.imageLable = World.doctor;
+        } else if (poiData.category.toUpperCase().includes("PLACE_OF_WORSHIP")) {
+            this.imageLable = World.temple;
+        } else if (poiData.category.toUpperCase().includes("RESIDENTIAL_ACCOMMODATION")) {
+            this.imageLable = World.residential;
+        } else if (poiData.category.toUpperCase().includes("GOVERNMENT_OFFICE")) {
+            this.imageLable = World.gov;
+        }
+        else if (poiData.category.toUpperCase().includes("BANK")) {
+            this.imageLable = World.bank;
+        } else if (poiData.category.toUpperCase().includes("SHOP")) {
+            this.imageLable = null;
+        } else if (poiData.category.toUpperCase().includes("CASH_DISPENSER")) {
+            this.imageLable = World.bank;
+        }
+        else if (poiData.category.toUpperCase().includes("SCHOOL")) {
+            this.imageLable = World.school;
+        }
+        else if (poiData.category.toUpperCase().includes("POST_OFFICE")) {
+            this.imageLable = World.post_office;
+        }else if (poiData.category.toUpperCase().includes("HOTEL_MOTEL")) {
+            this.imageLable = World.hotel;
+        }else if (poiData.category.toUpperCase().includes("AIRPORT")) {
+            this.imageLable = World.airport;
+        }else {
+            this.imageLable = World.default;
+        }
 
-    this.hotelRes = new AR.ImageDrawable(this.imageLable, 1.4, {
-        zOrder: 1,
-        opacity: 1.0,
-        verticalAnchor: AR.CONST.VERTICAL_ANCHOR.BOTTOM,
-        translate: {
-            x: -0.60,
-            y: -0.30
+        this.hotelRes = new AR.ImageDrawable(this.imageLable, 1.4, {
+            zOrder: 1,
+            opacity: 1.0,
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.BOTTOM,
+            translate: {
+                x: -0.60,
+                y: -0.30
 
-        },
-        onClick: Marker.prototype.getOnClickTrigger(this),
-    });
+            },
+            onClick: Marker.prototype.getOnClickTrigger(this),
+        });
 
-    // create an AR.Label for the marker's title
-    this.titleLabel = new AR.Label(poiData.title.trunc(10), 1, {
-        zOrder: 1,
-        translate: {
-            y: 0.40,
-            x: 0.05
-        },
-        horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.LEFT,
+        // create an AR.Label for the marker's title
+        this.titleLabel = new AR.Label(poiData.title.trunc(10), 1, {
+            zOrder: 1,
+            translate: {
+                y: 0.40,
+                x: 0.05
+            },
+            horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.LEFT,
+            style: {
+                textColor: '#FFFFFF',
+                backgroundColor: '#000000',
+                fontStyle: AR.CONST.FONT_STYLE.NORMAL
+            },
+            onClick: Marker.prototype.getOnClickTrigger(this),
+        });
+
+
+        // create an AR.Label for the marker's description
+        this.descriptionLabel = new AR.Label(poiData.description.trunc(15), 0.7, {
+            zOrder: 0,
+            translate: {
+                y: -0.55
+            },
+            style: {
+                textColor: '#FFFFFF',
+                backgroundColor: '#000000'
+            }
+        });
+        /*
+    Create an AR.ImageDrawable using the AR.ImageResource for the direction indicator which was created in the World. Set options regarding the offset and anchor of the image so that it will be displayed correctly on the edge of the screen.
+         */
+        this.directionIndicatorDrawable = new AR.ImageDrawable(World.markerDrawable_directionIndicator, 0.1, {
+            enabled: false,
+            verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP
+        });
+
+    /*
+        The representation of an AR.GeoObject in the radar is defined in its drawables set (second argument of AR.GeoObject constructor).
+        Once drawables.radar is set the object is also shown on the radar e.g. as an AR.Circle
+    */
+    this.radarCircle = new AR.Circle(0.03, {
+        horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.CENTER,
+        opacity: 0.8,
         style: {
-            textColor: '#FFFFFF',
-            backgroundColor: '#000000',
-            fontStyle: AR.CONST.FONT_STYLE.NORMAL
-        },
-        onClick: Marker.prototype.getOnClickTrigger(this),
-    });
-
-
-    // create an AR.Label for the marker's description
-    this.descriptionLabel = new AR.Label(poiData.description.trunc(15), 0.7, {
-        zOrder: 0,
-        translate: {
-            y: -0.55
-        },
-        style: {
-            textColor: '#FFFFFF',
-            backgroundColor: '#000000'
+            fillColor: "#ffffff"
         }
     });
-    /*
-Create an AR.ImageDrawable using the AR.ImageResource for the direction indicator which was created in the World. Set options regarding the offset and anchor of the image so that it will be displayed correctly on the edge of the screen.
-     */
-    this.directionIndicatorDrawable = new AR.ImageDrawable(World.markerDrawable_directionIndicator, 0.1, {
-        enabled: false,
-        verticalAnchor: AR.CONST.VERTICAL_ANCHOR.TOP
-    });
 
     /*
-     Create the AR.GeoObject with the drawable objects and define the AR.ImageDrawable as an indicator target on the marker AR.GeoObject. The   direction indicator is displayed automatically when necessary. AR.Drawable subclasses (e.g. AR.Circle) can be used as direction indicators.
-     */
+        Additionally create circles with a different color for the selected state.
+    */
+    this.radarCircleSelected = new AR.Circle(0.05, {
+        horizontalAnchor: AR.CONST.HORIZONTAL_ANCHOR.CENTER,
+        opacity: 0.8,
+        style: {
+            fillColor: "#0066ff"
+        }
+    });
+
+    this.radardrawables = [];
+    this.radardrawables.push(this.radarCircle);
+
+    this.radardrawablesSelected = [];
+    this.radardrawablesSelected.push(this.radarCircleSelected);
+
+    /*
+        Note that indicator and radar-drawables were added
+    */
     this.markerObject = new AR.GeoObject(markerLocation, {
         drawables: {
             cam: [this.titleLabel, this.hotelRes],
-            indicator: this.directionIndicatorDrawable
+            indicator: this.directionIndicatorDrawable,
+            radar: this.radardrawables
         }
     });
 
     return this;
 }
 
-// var highestRenderingOrder = 0;
-Marker.prototype.getOnClickTrigger = function (marker) {
+Marker.prototype.getOnClickTrigger = function(marker) {
 
     /*
-     The setSelected and setDeselected functions are prototype Marker functions.
-     Both functions perform the same steps but inverted.
-     */
+        The setSelected and setDeselected functions are prototype Marker functions.
+        Both functions perform the same steps but inverted.
+    */
 
-    return function () {
+    return function() {
 
         if (!Marker.prototype.isAnyAnimationRunning(marker)) {
             if (marker.isSelected) {
@@ -169,14 +198,13 @@ Marker.prototype.getOnClickTrigger = function (marker) {
 };
 
 /*
- Property Animations allow constant changes to a numeric value/property of an object, dependent on start-value, end-value and the duration of the animation. Animations can be seen as functions defining the progress of the change on the value. The Animation can be parametrized via easing curves.
- */
+    Property Animations allow constant changes to a numeric value/property of an object, dependent on start-value, end-value and the duration of the animation. Animations can be seen as functions defining the progress of the change on the value. The Animation can be parametrized via easing curves.
+*/
 
-Marker.prototype.setSelected = function (marker) {
+Marker.prototype.setSelected = function(marker) {
 
     marker.isSelected = true;
 
-    // New:
     if (marker.animationGroup_selected === null) {
 
         // create AR.PropertyAnimation that animates the opacity to 0.0 in order to hide the idle-state-drawable
@@ -231,13 +259,18 @@ Marker.prototype.setSelected = function (marker) {
 
     // enables the direction indicator drawable for the current marker
     marker.directionIndicatorDrawable.enabled = true;
+
+    marker.markerObject.drawables.radar = marker.radardrawablesSelected;
+
     // starts the selected-state animation
     marker.animationGroup_selected.start();
 };
 
-Marker.prototype.setDeselected = function (marker) {
+Marker.prototype.setDeselected = function(marker) {
 
     marker.isSelected = false;
+
+    marker.markerObject.drawables.radar = marker.radardrawables;
 
     if (marker.animationGroup_idle === null) {
 
@@ -295,7 +328,8 @@ Marker.prototype.setDeselected = function (marker) {
     marker.animationGroup_idle.start();
 };
 
-Marker.prototype.isAnyAnimationRunning = function (marker) {
+
+Marker.prototype.isAnyAnimationRunning = function(marker) {
 
     if (marker.animationGroup_idle === null || marker.animationGroup_selected === null) {
         return false;
@@ -309,6 +343,6 @@ Marker.prototype.isAnyAnimationRunning = function (marker) {
 };
 
 // will truncate all strings longer than given max-length "n". e.g. "foobar".trunc(3) -> "foo..."
-String.prototype.trunc = function (n) {
+String.prototype.trunc = function(n) {
     return this.substr(0, n - 1) + (this.length > n ? '...' : '');
 };
